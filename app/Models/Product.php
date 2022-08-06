e013fbfd80712c0933da5d59e6bf14a49a8ae53d
AYaichuk <a.yaychuk@gmail.com>
2022-08-06 13:07:58 +0300

    Laravel: UI (Bootstrap), Product Controller, Custom Request, Image Service Uploader

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
+use App\Services\FileStorageService;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
     use HasFactory;
-
     protected $fillable = [
         'category_id',
         'title',
@@ -21,22 +22,33 @@ class Product extends Model
         'in_stock',
         'SKU'
     ];
-
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
-
     public function images()
     {
         return $this->morphMany(Image::class, 'imageable');
     }
-
     public function available(): Attribute
     {
         return new Attribute(
             get: fn() => $this->attributes['in_stock'] > 0
         );
+    }
+
+    public function setThumbnailAttribute($image)
+    {
+        if (!empty($this->attributes['thumbnail'])) {
+            FileStorageService::remove($this->attributes['thumbnail']);
+        }
+
+        $this->attributes['thumbnail'] = FileStorageService::upload($image);
+    }
+
+    public function thumbnailUrl(): Attribute
+    {
+        return new Attribute(get: fn() => Storage::url($this->attributes['thumbnail']));
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,22 +13,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::name('admin.')->prefix('admin')->middleware(['auth', 'admin'])->group(function() {
     Route::get('/dashboard', function () {
         return view('dashboard', ['role' => 'Admin']);
     })->name('dashboard');
-
     Route::resource('products', \App\Http\Controllers\Admin\ProductsController::class)->except(['show']);
 });
-
-
 Route::get('/dashboard', function () {
     return view('dashboard', ['role' => 'Customer']);
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
