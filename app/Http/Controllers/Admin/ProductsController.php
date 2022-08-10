c648bc1e455c8c11f7c1da47a68070415d9153bf
AYaichuk <a.yaychuk@gmail.com>
2022-08-10 21:30:46 +0300

    Laravel: Admin Products and Categories CRUD

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -8,8 +8,10 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Services\Contracts\FileStorageServiceContract;
 use App\Services\FileStorageService;
+use App\Services\ImagesService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class ProductsController extends Controller
 {
@@ -43,8 +45,10 @@ class ProductsController extends Controller
             DB::beginTransaction();
 
             $data = $request->validated();
+            $images = $data['images'] ?? [];
             $category = Category::find($data['category']);
             $product = $category->products()->create($data); // category_id
+            ImagesService::attach($product, 'imageasfasfs', $images);
 
             DB::commit();
 
@@ -53,35 +57,58 @@ class ProductsController extends Controller
         } catch (\Exception $e) {
             DB::rollBack();
             logs()->warning($e);
-            return redirect()->back()->with('warn', 'Oops smth wrong. See logs');
+            return redirect()->back()->with('warn', 'Oops smth wrong. See logs')->withInput();
         }
     }
 
     /**
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Product $product
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-    public function edit($id)
+    public function edit(Product $product)
     {
-        //
+        $categories = Category::all();
+        return view('admin/products/edit', compact('product', 'categories'));
     }
 
     /**
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(UpdateProductRequest $request, Product $product)
     {
-        //
+        try {
+            DB::beginTransaction();
+
+            $data = $request->validated();
+            $thumbnail = $request->thumbnail != null ? FileStorageService::upload($request->thumbnail) : $product->thumbnail;
+            $images = $request->images ?? [];
+            $data['thumbnail'] = $thumbnail;
+            ImagesService::attach($product, 'images', $images);
+            Product::where('id', '=', $product->id)->update($data);
+
+            DB::commit();
+
+            return redirect()->route('admin.products.index')
+                ->with('status', "The product #{$product->id} was successfully updated!");
+        }catch (\Exception $e) {
+            DB::rollBack();
+            logs()->warning($e);
+            return redirect()->back()->with('warn', 'Oops smth wrong. See logs')->withInput();
+        }
     }
 
     /**
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function destroy($id)
+    public function destroy(Product $product)
     {
-        //
+        $product->images()->delete();
+        $product->delete();
+
+        return redirect()->route('admin.products.index')
+            ->with('status', "The product #{$product->id} deleted!");
     }
 }
